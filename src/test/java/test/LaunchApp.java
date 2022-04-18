ca632fd8a146c998ae75a5ed2fb2c94b21ac4ccf
lygiabao <baolg95@gmail.com>
2022-04-18 22:33:49 +0700

    lesson

---
+++ src/test/java/test/LaunchApp.java
@@ -0,0 +1,35 @@
+package test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class LaunchApp {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = null;
+        Exception exception = null;
+
+        try {
+            DesiredCapabilities desiredCaps = new DesiredCapabilities();
+            desiredCaps.setCapability("platformName", "Android");
+            desiredCaps.setCapability("automationName", "uiautomator2");
+            desiredCaps.setCapability("udid", "3300d3672cca62b9");
+            desiredCaps.setCapability("appPackage", "com.wdiodemoapp");
+            desiredCaps.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+
+            URL appiumServer = new URL("http://localhost:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCaps);
+            Thread.sleep(3000);
+        } catch (Exception e){
+            exception = e;
+        }
+
+        if(appiumDriver == null){
+            throw new RuntimeException(exception.getMessage());
+        }
+
+    }
+}
